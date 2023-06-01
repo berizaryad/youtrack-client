c634a8c84bc6262365ab625a4453ac028c0c4054
Danil <danil@pfm.team>
2023-06-01 16:09:04 +0300

    Omitempties added

--- entities.go
+++ entities.go
@@ -6,45 +6,45 @@ package youtrack
 // Project entity.
 // Source: https://www.jetbrains.com/help/youtrack/devportal/api-entity-Project.html
 type Project struct {
-	Name string `json:"name"`
+	Name string `json:"name,omitempty"`
 }
 
 // IssuesCountResponse entity.
 // Source: https://www.jetbrains.com/help/youtrack/devportal/api-entity-IssueCountResponse.html
 type IssuesCountResponse struct {
-	Count int `json:"count"`
+	Count int `json:"count,omitempty"`
 }
 
 // Issue entity.
 // Source: https://www.jetbrains.com/help/youtrack/devportal/api-entity-Issue.html
 type Issue struct {
-	ID           string             `json:"id"`
-	IDReadable   string             `json:"idReadable"`
-	Summary      string             `json:"summary"`
-	Project      Project            `json:"project"`
-	CustomFields []IssueCustomField `json:"customFields"`
+	ID           string             `json:"id,omitempty"`
+	IDReadable   string             `json:"idReadable,omitempty"`
+	Summary      string             `json:"summary,omitempty"`
+	Project      Project            `json:"project,omitempty"`
+	CustomFields []IssueCustomField `json:"customFields,omitempty"`
 }
 
 // IssueCustomField entity.
 // Source: https://www.jetbrains.com/help/youtrack/devportal/api-entity-IssueCustomField.html
 type IssueCustomField struct {
-	Name  string `json:"name"`
-	Value any    `json:"value"`
+	Name  string `json:"name,omitempty"`
+	Value any    `json:"value,omitempty"`
 }
 
 // PeriodValue entity.
 // https://www.jetbrains.com/help/youtrack/devportal/api-entity-PeriodValue.html
 type PeriodValue struct {
-	ID           string `json:"id"`
-	Minutes      int    `json:"minutes"`
-	Presentation string `json:"presentation"`
+	ID           string `json:"id,omitempty"`
+	Minutes      int    `json:"minutes,omitempty"`
+	Presentation string `json:"presentation,omitempty"`
 }
 
 // ActivityItem entity.
 // Source: https://www.jetbrains.com/help/youtrack/devportal/api-entity-ActivityItem.html
 type ActivityItem struct {
-	Added     any   `json:"added"`
-	Timestamp int64 `json:"timestamp"`
+	Added     any   `json:"added,omitempty"`
+	Timestamp int64 `json:"timestamp,omitempty"`
 }
 
 // User entity.
@@ -58,42 +58,42 @@ type User struct {
 // IssueWorkItem entity.
 // Source: https://www.jetbrains.com/help/youtrack/devportal/api-entity-IssueWorkItem.html
 type IssueWorkItem struct {
-	Author   User          `json:"author"`
-	Duration DurationValue `json:"duration"`
+	Author   User          `json:"author,omitempty"`
+	Duration DurationValue `json:"duration,omitempty"`
 }
 
 // DurationValue entity.
 // Source: https://www.jetbrains.com/help/youtrack/devportal/api-entity-DurationValue.html
 type DurationValue struct {
-	Minutes int `json:"minutes"`
+	Minutes int `json:"minutes,omitempty"`
 }
 
 // Hub entities
 // Source: https://www.jetbrains.com/help/youtrack/devportal/HUB-REST-API_JSON-Scheme.html
 
 type HubUsers struct {
-	Type  string    `json:"type"`
-	Next  string    `json:"next"`
-	Skip  int       `json:"skip"`
-	Top   int       `json:"top"`
-	Users []HubUser `json:"users"`
+	Type  string    `json:"type,omitempty"`
+	Next  string    `json:"next,omitempty"`
+	Skip  int       `json:"skip,omitempty"`
+	Top   int       `json:"top,omitempty"`
+	Users []HubUser `json:"users,omitempty"`
 }
 
 // HubUser is a "user" entity from hub api.
 // Source: https://www.jetbrains.com/help/youtrack/devportal/HUB-REST-API_JSON-Scheme.html#user
 type HubUser struct {
-	Login string `json:"login"`
+	Login string `json:"login,omitempty"`
 }
 
 type HubProjects struct {
-	Skip     int          `json:"skip"`
-	Top      int          `json:"top"`
-	Total    int          `json:"total"`
-	Projects []HubProject `json:"projects"`
+	Skip     int          `json:"skip,omitempty"`
+	Top      int          `json:"top,omitempty"`
+	Total    int          `json:"total,omitempty"`
+	Projects []HubProject `json:"projects,omitempty"`
 }
 
 // HubProject is a "project" entity from hub api.
 // Source: https://www.jetbrains.com/help/youtrack/devportal/HUB-REST-API_JSON-Scheme.html#project
 type HubProject struct {
-	Name string `json:"name"`
+	Name string `json:"name,omitempty"`
 }
